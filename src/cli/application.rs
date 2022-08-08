cc7448cadacbbef3ebfd648432121a802d61b10b
Tobias Nilsson <tobias.jens.nilsson@gmail.com>
2022-08-08 19:32:10 +0200

    feat: Allow cli to configure indentation

--- src/cli/application.rs
+++ src/cli/application.rs
@@ -5,7 +5,6 @@ use clap::AppSettings;
 use clap::ArgAction;
 
 use crate::cli::application_request::ApplicationRequest;
-use crate::junit_merger::config::Config;
 use crate::junit_merger::JunitMerger;
 
 use super::output_writer::OutputWriter;
@@ -28,6 +27,26 @@ impl<'help> Application<'help> {
                 arg!(-f --force "No error if output file exists, overwrites content")
                     .action(ArgAction::SetTrue),
             )
+            .arg(
+                clap::Arg::new("indent-size")
+                    .long("indent-size")
+                    .short('s')
+                    .help("Number of indentation characters")
+                    .takes_value(true)
+                    .value_name("INT")
+                    .value_parser(clap::value_parser!(usize))
+                    .default_value("3"),
+            )
+            .arg(
+                clap::Arg::new("indent-character")
+                    .long("indent-character")
+                    .short('c')
+                    .help("Whitespace character to use for indentation")
+                    .takes_value(true)
+                    .value_name("CHAR")
+                    .value_parser(clap::value_parser!(char))
+                    .default_value(" "),
+            )
             .setting(AppSettings::ArgRequiredElseHelp)
             .disable_colored_help(true);
         Self { clap }
@@ -41,7 +60,8 @@ impl<'help> Application<'help> {
     }
 
     fn run_with_request(request: ApplicationRequest) -> Result<()> {
-        let mut merger = JunitMerger::from_paths_and_config(request.paths, Config::default())?;
+        let config = request.config();
+        let mut merger = JunitMerger::from_paths_and_config(request.paths, config)?;
         let result = merger.merge_into()?;
         OutputWriter::write(&result, request.output_path)
     }
--- src/cli/application_request.rs
+++ src/cli/application_request.rs
@@ -4,20 +4,42 @@ use clap::ArgMatches;
 use std::path::Path;
 
 use crate::cli::output_path_parser::OutputPathParser;
+use crate::junit_merger::config::Config;
 
 #[derive(Debug)]
 pub struct ApplicationRequest<'a> {
     pub paths: ValuesRef<'a, String>,
     pub output_path: Option<&'a Path>,
+    pub indent_size: usize,
+    pub indent_character: u8,
 }
 
 impl<'a> ApplicationRequest<'a> {
     pub fn from_matches(matches: &'a ArgMatches) -> Result<Option<Self>> {
         Ok(if let Some(paths) = matches.get_many::<String>("FILE") {
             let output_path = OutputPathParser::from_matches(matches).parse()?;
-            Some(ApplicationRequest { paths, output_path })
+            let indent_size = *matches
+                .get_one::<usize>("indent-size")
+                .expect("--indent-size has a default value.");
+            let indent_character: char = *matches
+                .get_one::<char>("indent-character")
+                .expect("--indent-character has a default value.");
+            let indent_character = indent_character.try_into()?;
+            Some(ApplicationRequest {
+                paths,
+                output_path,
+                indent_size,
+                indent_character,
+            })
         } else {
             None
         })
+    }
+
+    pub fn config(&self) -> Config {
+        Config {
+            indent_char: self.indent_character,
+            indent_size: self.indent_size,
+        }
     }
 }
